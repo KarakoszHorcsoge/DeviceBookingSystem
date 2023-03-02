934b3740847995cf3f7fbe4be2ebc11981ee454d
Németh Dániel <nemeth.daniel@sze.hu>
2023-03-02 22:55:50 +0100

    expendable table finally

--- kolcsonzo_server/src/app/custom-table/custom-table.component.ts
+++ kolcsonzo_server/src/app/custom-table/custom-table.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 
 export interface PeriodicElement {
@@ -15,15 +15,21 @@ export interface PeriodicElement {
   styleUrls: ['./custom-table.component.css'],
   template:
     `msg: {{msg}}`,
+    animations: [
+      trigger('detailExpand', [
+        state('collapsed', style({height: '0px', minHeight: '0'})),
+        state('expanded', style({height: '*'})),
+        transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      ]),
+    ],
 })
 export class CustomTableComponent implements OnInit {
   //inputok
   @Input() load!: Observable<any>;
   @Input() contentsToShow!: string[];
   @Input() displayNames!: string[];
-  @Input() tooltip: string = "";
-  @Input() descriptionsDisplayName: string[] = ["expand"];
-  
+  @Input() descriptionsDisplayName: string[] = [];
+  @Input() descriptions: string[] = [];
 
   //variables
   contents: any[] = [];
@@ -43,7 +49,7 @@ export class CustomTableComponent implements OnInit {
       this.columnsWithDescription = [...this.displayNames,...this.descriptionsDisplayName]
 
       //this.displayNames.push(this.descriptionsDisplayName[0]);
-      //this.contentsToShow = this.contentsToShow.concat(this.descriptionsDisplayName)
+      //this.displayNames = this.displayNames.concat(this.descriptionsDisplayName)
   }
 
 }
