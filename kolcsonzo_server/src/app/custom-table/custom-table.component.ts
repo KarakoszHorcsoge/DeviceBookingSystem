9d6f5b9bc33934ddf5b25146617ab38363ca266c
Németh Dániel <nemeth.daniel@sze.hu>
2023-03-04 22:41:47 +0100

    fix sorting

--- kolcsonzo_server/src/app/custom-table/custom-table.component.ts
+++ kolcsonzo_server/src/app/custom-table/custom-table.component.ts
@@ -72,16 +72,4 @@ export class CustomTableComponent implements OnInit {
   getPageSizeOptions():number[]{
     return [10,20,50,100,this.length].filter(x => x <=  this.length);
   }
-
-  announceSortChange(sortState: Sort) {
-    // This example uses English messages. If your application supports
-    // multiple language, you would internationalize these strings.
-    // Furthermore, you can customize the message to add additional
-    // details about the values being sorted.
-    if (sortState.direction) {
-      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-    } else {
-      this._liveAnnouncer.announce('Sorting cleared');
-    }
-  }
 }
